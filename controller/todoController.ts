8af0ad7052445efefbcc1256a7ab85ddd32c669e
kamal1234567890 <kamaluddin1623@gmail.com>
2023-01-24 00:51:19 +0800

    create method getTodoById

--- controller/todoController.ts
+++ controller/todoController.ts
@@ -1,25 +1,52 @@
 import { Response, Request } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { Todos } from '../data/todos.ts'
+import Todo from "../interfaces/Todo.ts";
 
+// create type req, res
 type myCtx = {
-    request?: Request,
-    response: Response
+    request: Request,
+    response: Response,
+    params: {id:string}
 };
 
 export default {
 
-    getAllBooks({response}: myCtx){
+    getAllTodos({response}: myCtx): void{
+        response.status = 200;
+        response.body = {
+            success: true,
+            data: Todos
+        };
+        return;
+    },
+
+    getTodoById({params, response}: myCtx): void{
+        const id: string = params.id;
+        
+        const todo: Todo | undefined = Todos.find((t: Todo):boolean => t.id === id);
+        if (!todo) {
+            response.status = 404;
+            response.body = {
+                success: false,
+                message: "todo not found"
+            }
+            return;
+        }
+
+        response.status = 200;
+        response.body = {
+            success : true,
+            data: todo
+        }
+        return;
+    },
+    createTodo({response}: myCtx){
         response.status
     },
-    getBookById({response}: myCtx){
+    updateTodo({response}: myCtx){
         response.status
     },
-    createBook({response}: myCtx){
-        response.status
-    },
-    updateBook({response}: myCtx){
-        response.status
-    },
-    deleteBook({response}: myCtx){
+    deleteTodo({response}: myCtx){
         response.status
     }
 }

d500d83792616020c84c1ba721682e008d4cc58a
kamal1234567890 <kamaluddin1623@gmail.com>
2023-01-27 06:13:04 +0800

    deploy

--- server.ts
+++ server.ts
@@ -3,21 +3,6 @@ import todoRouter from './routes/todo.ts'
 
 const app = new Application();
 
-// Logger
-app.use(async (ctx: Context, next): Promise<void> => {
-    await next();
-    const rt: string | null = ctx.response.headers.get("X-Response-Time");
-    console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
-});
-
-// Timing
-app.use(async (ctx: Context, next): Promise<void> => {
-    const start: number = Date.now();
-    await next();
-    const ms: number = Date.now() - start;
-    ctx.response.headers.set("X-Response-Time", `${ms}ms`);
-});
-
 // make direktory
 app.use(async (_ctx: Context, next): Promise<void> => {
     if (!existsSync("./data")) await Deno.mkdir("data");
